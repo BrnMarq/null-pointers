ae101bce214fdfc1f4d091810047e0b5cc19c64a
BrnMarq <brnmarq@gmail.com>
2025-05-08 13:12:49 -0400

    🚧 Add Task class

---
+++ lib/include/Task.hpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <ctime>
+
+#include "Types.hpp"
+
+class Task
+{
+public:
+private:
+  // * Logic info
+  time_t estimated_time;
+  time_t dead_line;
+  // TODO how to get deparment to be enum
+  std::string department;
+  level difficulty;
+
+  // * Extra info
+  std::string title;
+  std::string requirements;
+};
