0c189fa79e8f2ddba0a07eba0e9fdf86b873bb56
mafer1707 <mafercasmen@gmail.com>
2025-05-13 15:21:47 -0400

    ✨InvalidCommandException Added

--- lib/include/Definitions.hpp
+++ lib/include/Definitions.hpp
@@ -1,4 +1,7 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 enum class level
 {
@@ -14,3 +17,7 @@ struct Configs
   static float time_gap_multiplier;
   static float difficulty_gap_multiplier;
 };
+
+const std::vector<std::string> valid_program_args = {"np"};
+
+bool is_valid_argument(const std::string &arg);
--- lib/src/Definitions.cpp
+++ lib/src/Definitions.cpp
@@ -2,3 +2,8 @@
 
 float Configs::time_gap_multiplier{1.0f};
 float Configs::difficulty_gap_multiplier{1.0f};
+
+bool is_valid_argument(const std::string &arg)
+{
+    return std::find(valid_program_args.begin(), valid_program_args.end(), arg) != valid_program_args.end();
+}
