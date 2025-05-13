18b9f7d6b9055b5622bec7b807c3ae590f5b8511
mafer1707 <mafercasmen@gmail.com>
2025-05-13 19:21:12 -0400

    🎉np command implementation

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <typeinfo>
+#include <chrono>
 
 #include "json.hpp"
 #include <Agent.hpp>
@@ -10,17 +11,15 @@
 
 using json = nlohmann::json;
 
-level get_level_from_string(const std::string &_level)
+std::chrono::sys_days string_to_date(const std::string &date)
 {
-  std::unordered_map<std::string, level> level_map =
-      {
-          {"novice", level::novice},
-          {"beginner", level::beginner},
-          {"competent", level::competent},
-          {"proficient", level::proficient},
-          {"expert", level::expert}};
+  std::istringstream iss(date);
 
-  return level_map.at(_level);
+  int year, month, day;
+  char sep;
+  iss >> year >> sep >> month >> sep >> day;
+
+  return std::chrono::sys_days{std::chrono::year(year) / std::chrono::month(month) / std::chrono::day(day)};
 }
 
 int main(int argc, char *argv[])
@@ -44,24 +43,64 @@ int main(int argc, char *argv[])
       }
     }
   }
-  // std::ifstream file("../../.np/agents.json");
-  // json agents = json::parse(file);
 
-  // std::vector<Agent> agents_list;
+  std::ifstream file_agents("../../.np/agents.json");
+  std::ifstream file_task("../../.np/tasks.json");
 
-  // for (const auto &item : agents)
-  // {
-  //   std::chrono::hours _available_time(item["available_time"].get<int>());
-  //   std::string _deparment = item["department"].get<std::string>();
-  //   level _level = get_level_from_string(item["expertise"].get<std::string>());
-  //   std::string _name = item["name"].get<std::string>();
+  json agents = json::parse(file_agents);
+  json tasks = json::parse(file_task);
 
-  //   Agent agent(_available_time, _deparment, _level, _name);
+  std::vector<Agent> agents_list;
+  std::vector<Task> task_list;
 
-  //   agents_list.push_back(agent);
-  // }
+  // std::string fecha = "2025-08-09";
+  // auto y = string_to_date(fecha);
+
+  for (const auto &ag : agents)
+  {
+    std::chrono::hours _available_time(ag["available_time"].get<int>());
+    std::string _deparment = ag["department"].get<std::string>();
+
+    if (!department_exists(_deparment))
+    {
+      Departments::departments.push_back(_deparment);
+    }
+
+    level _level = get_level_from_string(ag["expertise"].get<std::string>());
+    std::string _name = ag["name"].get<std::string>();
+
+    Agent agent(_available_time, _deparment, _level, _name);
+
+    agents_list.push_back(agent);
+  }
+
+  //  std::chrono::hours estimated_time;
+  // std::chrono::sys_days dead_line;
+  // // TODO how to get deparment to be enum
+  // std::string department;
+  // level difficulty;
+
+  // // * Extra info
+  // std::string title;
+  // std::string requirements;
+
+  for (const auto &tsk : tasks)
+  {
+    std::chrono::hours _estimated_time(tsk["estimated_time"].get<int>());
+    std::chrono::sys_days _dead_line = string_to_date(tsk["dead_line"].get<std::string>());
+    std::string _deparment = tsk["department"].get<std::string>();
+
+    level _difficulty = get_level_from_string(tsk["difficulty"].get<std::string>());
+
+    std::string _title = tsk["title"].get<std::string>();
+
+    Task task(_estimated_time, _dead_line, _deparment, _difficulty, _title, "");
+    task_list.push_back(task);
+  };
 
   // std::cout << agents_list.size() << std::endl;
+  // std::cout << task_list.size() << std::endl;
+  // std::cout << Departments::departments.size() << std::endl;
 
   std::chrono::sys_days now{floor<std::chrono::days>(std::chrono::system_clock::now())};
 
