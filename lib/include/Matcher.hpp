f8e9914bc3be62dc6b536244731c50612a91f537
BrnMarq <brnmarq@gmail.com>
2025-05-12 22:23:42 -0400

    🚧 Add placeholder function for create_match function

--- lib/include/Matcher.hpp
+++ lib/include/Matcher.hpp
@@ -10,10 +10,15 @@ public:
   using AgentVectorT = std::vector<Agent>;
   using TaskVectorT = std::vector<Task>;
   using ATArcVectorT = std::vector<Arc<Agent, Task>>;
+  using MatchT = std::unordered_map<Agent, TaskVectorT>;
 
   const AgentVectorT &get_agents() const noexcept;
+
   const TaskVectorT &get_tasks() const noexcept;
+
   const ATArcVectorT &get_arcs() const noexcept;
+
+  MatchT create_match() noexcept;
 
 private:
   AgentVectorT agents;
