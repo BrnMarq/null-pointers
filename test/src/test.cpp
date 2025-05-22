d34144db1034854e87ff6a5101b85e20aea0d737
BrnMarq <brnmarq@gmail.com>
2025-05-22 18:55:44 -0400

    ✅ Update tests: Fill agents and tasks

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,3 +1,27 @@
-int main() {
-  return 0;
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <string>
+
+std::string get_agent_path(int i)
+{
+  std::filesystem::path agents_path = std::filesystem::current_path() / "resources/agent";
+  return agents_path.generic_string() + std::to_string(i) + std::string(".txt");
+}
+
+std::string get_task_path(int i)
+{
+  std::filesystem::path tasks_path = std::filesystem::current_path() / "resources/task";
+  return tasks_path.generic_string() + std::to_string(i) + std::string(".txt");
+}
+
+int main()
+{
+  for (int i = 1; i < 6; ++i)
+  {
+    system((std::string("./main/bin/main add_agent < ") + get_agent_path(i)).c_str());
+    system((std::string("./main/bin/main add_task < ") + get_task_path(i)).c_str());
+  }
+
+  return EXIT_SUCCESS;
 }
